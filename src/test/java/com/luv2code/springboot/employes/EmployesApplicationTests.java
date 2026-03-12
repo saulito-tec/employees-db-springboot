32f962ee8f9e0d9615510b9605e56a5f5f671b92
Saúl <147526498+saulito-tec@users.noreply.github.com>
2026-03-12 09:30:08 -0600

    initial commit

---
+++ src/test/java/com/luv2code/springboot/employes/EmployesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.employes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
